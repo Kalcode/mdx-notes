0af5b60b1376821b5914a7e3a67b198afdc26169
David Clausen <davidclausen2051@gmail.com>
2019-09-21 01:09:59 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './utils/ReactComponentHandlerPatcher';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
